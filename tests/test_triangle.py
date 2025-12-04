608efd220ef584fe0053802c00f520c977e762bd
Ma-Mush <alya.hahaha.ivanova@mail.ru>
2025-12-04 14:21:32 +0300

    triangle bug fixed

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -3,20 +3,20 @@ from src.triangle import area, perimeter
 
 class RectangleTestCase(unittest.TestCase):   
     def test_default_perimeter(self):
-        res = perimeter(8, 10)
-        self.assertEqual(res, 36)
+        res = perimeter(3, 4, 5)
+        self.assertEqual(res, 12)
 
     def test_zero_perimeter(self):
-        res = perimeter(0, 0)
+        res = perimeter(0, 0, 0)
         self.assertEqual(res, 0)    
 
     def test_float_perimeter(self):
-        res = perimeter(3.5, 2.5)
-        self.assertAlmostEqual(res, 12.0)
+        res = perimeter(3.5, 2.5, 5.5)
+        self.assertAlmostEqual(res, 11.5)
     
     def test_default_area(self):
         res = area(8, 10)
-        self.assertEqual(res, 80)
+        self.assertEqual(res, 40)
 
     def test_zero_area(self):
         res = area(10, 0)
@@ -24,7 +24,7 @@ class RectangleTestCase(unittest.TestCase):
     
     def test_float_area(self):
         res = area(3.5, 2.5)
-        self.assertAlmostEqual(res, 8.75)
+        self.assertAlmostEqual(res, 4.375)
 
 if __name__ == '__main__':
     unittest.main()
